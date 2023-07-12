0cfaa7c8c0b605fe76461ec0fec989faa3bea991
Jade Wright <iamjwright159@gmail.com>
2023-07-12 18:33:08 -0400

    Add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Panel from "./components/panel"
+
+export default function NotFound()
+{
+	return (
+		<Panel
+			title="404 - Not Found"
+		>
+			The requested resource was not found on the server.
+		</Panel>
+	)
+}
