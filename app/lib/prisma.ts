c356d27862718ad351d3a6a76dac0f7f8eed912b
Jade Wright <iamjwright159@gmail.com>
2023-07-17 14:01:49 -0400

    Add dev db

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,15 +1,20 @@
 import { PrismaClient } from '@prisma/client'
+import * as dotenv from 'dotenv'
 
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production')
 {
+	dotenv.config({ path: 'prisma/.env.prod' })
 	prisma = new PrismaClient()
 }
 else
 {
 	if (!global.prisma)
+	{
+		dotenv.config({ path: 'prisma/.env.dev' })
 		global.prisma = new PrismaClient()
+	}
 	
 	prisma = global.prisma
 }
