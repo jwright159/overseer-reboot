95abd71eca80010fa986d44c42bad003688a8300
Jade Wright <iamjwright159@gmail.com>
2023-07-31 17:47:58 -0400

    Put setReferrer in a useEffect instead

--- app/referrer-context-provider.tsx
+++ app/referrer-context-provider.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Dispatch, ReactNode, SetStateAction, useCallback, useState } from "react"
+import { Dispatch, ReactNode, SetStateAction, useCallback, useEffect, useState } from "react"
 import { ReferrerContext } from "@/lib/context"
 import { usePathname } from "next/navigation"
 
@@ -11,11 +11,10 @@ export default function ReferrerContextProvider({
 })
 {
 	const [referrer, setReferrer] = useState("/overview")
-	const setReferrerCallback = useCallback(setReferrer, [setReferrer])
 
 	return (
 		<>
-			<ReferrerContextProviderProvider setReferrer={setReferrerCallback}/>
+			<ReferrerContextProviderProvider setReferrer={setReferrer}/>
 			<ReferrerContext.Provider value={referrer}>
 				{children}
 			</ReferrerContext.Provider>
@@ -31,14 +30,17 @@ function ReferrerContextProviderProvider({
 {
 	const pathname = usePathname()
 
-	if (!(
-		pathname === "/login" ||
-		pathname === "/register" ||
-		pathname === "/select-character" ||
-		pathname === "/register-character" ||
-		pathname === "/register-session"
-	))
-		setReferrer(pathname)
+	useEffect(() =>
+	{
+		if (!(
+			pathname === "/login" ||
+			pathname === "/register" ||
+			pathname === "/select-character" ||
+			pathname === "/register-character" ||
+			pathname === "/register-session"
+		))
+			setReferrer(pathname)
+	})
 	
 	return <></>
 }
