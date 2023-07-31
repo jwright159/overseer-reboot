85317ed7829dbed0e92bee33d4b57b3789d096e4
Jade Wright <iamjwright159@gmail.com>
2023-07-31 16:55:40 -0400

    Remove redundant root layout for 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,6 @@
 import "./globals.css"
 import MainPanel from "./components/main-panel"
 import { headers } from "next/headers"
-import RootLayout from "./layout"
 import GameLayout from "./(menu)/layout"
 
 export default function NotFound()
@@ -9,14 +8,12 @@ export default function NotFound()
 	const header = headers()
 
 	return (
-		<RootLayout>
-			<GameLayout>
-				<MainPanel
-					title={`404 - ${header.get("x-invoke-path")} Not Found`}
-				>
-					The requested resource was not found on the server.
-				</MainPanel>
-			</GameLayout>
-		</RootLayout>
+		<GameLayout>
+			<MainPanel
+				title={`404 - ${header.get("x-invoke-path")} Not Found`}
+			>
+				The requested resource was not found on the server.
+			</MainPanel>
+		</GameLayout>
 	)
 }
